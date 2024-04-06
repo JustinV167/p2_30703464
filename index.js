fae7fbf8a5b7936c10e5417632ed357b552aa146
JustinV167 <justinvegas14@gmail.com>
2024-04-05 20:56:33 -0400

    [ADD]estableciendo proyecto

---
+++ index.js
@@ -0,0 +1,18 @@
+const express= require("express")
+const app=express()
+
+const PORT=process.env.PORT ||3000 
+app.use(express.static(__dirname+"/public"))
+app.set("view engine","ejs")
+app.set("views", __dirname +"/Views")
+
+//estableciendo las rutas de la pagina
+app.use("/", require("./router/index"))
+app.use((req,res)=>{
+	res.status(404).render("error_404.ejs")
+})
+
+
+app.listen(PORT,()=>{
+	console.log("El puerto a entrar es:", PORT)
+})
