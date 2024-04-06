62a38429f47f196b0ff76f5efe6e05b47f560bef
JustinV167 <justinvegas14@gmail.com>
2024-04-06 12:59:04 -0400

    [add]agregando comentarios

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+//importando express
 const express= require("express")
 const app=express()
-
+//estableciendo puerto, por env y si no existe, que se ejecute en el 3000
 const PORT=process.env.PORT ||3000 
+//estableciendo carpeta publica
 app.use(express.static(__dirname+"/public"))
+//estableciendo motor grafico
 app.set("view engine","ejs")
+//estableciendo carpeta de las vistas
 app.set("views", __dirname +"/views")
 
 //estableciendo las rutas de la pagina
 app.use("/", require("./router/index"))
+//por si hay error
 app.use((req,res)=>{
 	res.status(404).render("error_404.ejs")
 })
 
-
+//iniciando servidor
 app.listen(PORT,()=>{
 	console.log("El puerto a entrar es:", PORT)
 })
--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const router= express.Router()
 
 router.get("/",(req,res)=>{
 	
-
+	//estableciendo vista para index
 	res.render("index.ejs")
 
 })
